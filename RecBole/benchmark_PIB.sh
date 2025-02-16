4d8e3970ad74b7f120126f4b8dbb95539e6e7c73
Nickstree <hsnu.nick@gmail.com>
2025-02-16 07:15:42 +0000

    upload

---
+++ RecBole/benchmark_PIB.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# # Define parameters
+models=("SASRec" "BERT4Rec")
+learning_rates=(1e-3 5e-4 1e-4)
+n_heads=(1 2 4)
+datasets=("amazon-beauty" "amazon-sports-outdoors" "amazon-toys-games")
+config_file="test.yaml"
+
+for dataset_name in "${datasets[@]}"; do
+  for model in "${models[@]}"; do
+    for lr in "${learning_rates[@]}"; do
+      for heads in "${n_heads[@]}"; do
+        echo "Running: Model=$model, Learning Rate=$lr, n_heads=$heads"
+        python run_recbole.py \
+          --model="$model" \
+          --dataset="$dataset_name" \
+          --config_files="$config_file" \
+          --learning_rate="$lr" \
+          --n_heads="$heads" \
+          --NoPos=True \
+          --PIB=True 
+      done
+    done
+  done
+done
